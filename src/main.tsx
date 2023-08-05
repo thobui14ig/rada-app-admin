9987176742c5d85084f4aabd927b9a89a18a37c5
lesythanh <lesythanh063@gmail.com>
2023-08-05 23:03:58 +0700

    layout Admin

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import './index.css';
 import './satoshi.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
