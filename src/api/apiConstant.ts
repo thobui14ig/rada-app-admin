f271af976a4dd2f2764287bab38dd920b1fa991a
thobui14ig <buithanhtho14ig@gmail.com>
2023-08-13 16:16:58 +0700

    order

--- src/api/apiConstant.ts
+++ src/api/apiConstant.ts
@@ -1,6 +1,7 @@
 const ApiConstant = {
   IP: 'chungta.vanhanhhethong.com',
   BASE_API_URL: `http://localhost:3000/`,
+  // BASE_API_URL: `https://buithanhtho.name.vn/`,
 };
 
 export default ApiConstant;
--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -5,7 +5,10 @@ export class SocketSingleton{
     socket: SocketType;
 
     constructor() {
-        this.socket = io(ApiConstant.BASE_API_URL);
+        this.socket = io(ApiConstant.BASE_API_URL, {
+            query: { token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInBob25lIjoiMDgyMjQyMzI0NiIsImlhdCI6MTY5MTkxMTYyOSwiZXhwIjoxNjkxOTk4MDI5fQ.ekwGdTmW-9B-2MteKJbi6Z__JRA1cVjCQG1AsOkXUrU` },
+            secure: true,
+          });
         this.socket.on('connect', () => {
           console.log('Connected to server admin page!.');
         });
