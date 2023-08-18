48998f2cd160887be147510dc114f6e33e6c68ab
thobui14ig <buithanhtho14ig@gmail.com>
2023-08-18 12:44:28 +0700

    fix socket

--- src/api/apiConstant.ts
+++ src/api/apiConstant.ts
@@ -1,5 +1,7 @@
 const ApiConstant = {
-  // BASE_API_URL: `http://localhost:3000/`,
+  // URL: 'http://localhost:3000',
+  // BASE_API_URL: `http://localhost:3000/api`,
+  URL: 'https://buithanhtho.name.vn',
   BASE_API_URL: `https://buithanhtho.name.vn/api/`,
 };
 
--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -5,18 +5,25 @@ export class SocketSingleton{
     socket: SocketType;
 
     constructor() {
-        this.socket = io(ApiConstant.BASE_API_URL, {
-            query: { token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInBob25lIjoiMDgyMjQyMzI0NiIsImlhdCI6MTY5MTkxMTYyOSwiZXhwIjoxNjkxOTk4MDI5fQ.ekwGdTmW-9B-2MteKJbi6Z__JRA1cVjCQG1AsOkXUrU` },
-            secure: true,
-          });
+     
+        this.socket = io(ApiConstant.URL);
+        console.log(123123123, this.socket)
         this.socket.on('connect', () => {
           console.log('Connected to server admin page!.');
+        });
+        this.socket.on('error', (error) => {
+          console.error('Socket connection error:', error);
+        });
+        this.socket.on('disconnect', () => {
+          console.log('Disconnected from socket');
         });
     }
 
     getSocket() {
         return this.socket;
     }
+
+    
 }
 
 const socketObj = new SocketSingleton()
